1c98180b42acaec1fd95fab9ccb431211aea25e9
Claudio Greco <claudio.greco@unitn.it>
2018-06-02 12:18:02 +0200

    1) Added Foil 1.0 dataset; 2) Added preprocessing script for the Foil dataset; 3) Added Bottom-Up and Top-Down Attention model for the first task of the Foil dataset.

---
+++ datasets.py
@@ -0,0 +1,48 @@
+import csv
+import json
+
+import numpy as np
+
+from utils import pad_sequences
+
+
+def load_foil_dataset(filename, token2id, label2id):
+    labels = []
+    padded_sentences = []
+    image_names = []
+    original_sentences = []
+
+    with open(filename) as in_file:
+        reader = csv.reader(in_file, delimiter="\t")
+
+        for row in reader:
+            label = row[0].strip()
+            sentence_tokens = row[2].strip().split()
+            image = row[3].strip().split("#")[0]
+            hypothesis = row[5].strip()
+            labels.append(label2id[label])
+            padded_sentences.append([token2id.get(token, token2id["#unk#"]) for token in sentence_tokens])
+            image_names.append(image)
+            original_sentences.append(hypothesis)
+
+        padded_sentences = pad_sequences(padded_sentences, padding="post", value=token2id["#pad#"], dtype=np.long)
+        labels = np.array(labels)
+
+    return labels, padded_sentences, image_names, original_sentences
+
+
+class ImageReader:
+    def __init__(self, img_names_filename, img_features_filename):
+        self._img_names_filename = img_names_filename
+        self._img_features_filename = img_features_filename
+
+        with open(img_names_filename) as in_file:
+            img_names = json.load(in_file)
+
+        with open(img_features_filename, mode="rb") as in_file:
+            img_features = np.load(in_file)
+
+        self._img_names_features = {filename: features for filename, features in zip(img_names, img_features)}
+
+    def get_features(self, images_names):
+        return np.array([self._img_names_features[image_name] for image_name in images_names])
---
+++ preprocess_foil_dataset.py
@@ -0,0 +1,29 @@
+import csv
+import json
+from argparse import ArgumentParser
+
+import spacy
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--foil_split_filename", type=str, required=True)
+    parser.add_argument("--preprocessed_foil_split_filename", type=str, required=True)
+    args = parser.parse_args()
+
+    images = {}
+
+    with open(args.foil_split_filename) as in_file:
+        dataset = json.load(in_file)
+        nlp = spacy.load("en_core_web_sm")
+
+        for image in dataset["images"]:
+            images[image["id"]] = image["file_name"]
+
+        with open(args.preprocessed_foil_split_filename, mode="w") as out_file:
+            writer = csv.writer(out_file, delimiter="\t")
+
+            for annotation in dataset["annotations"]:
+                caption = [token.lower for token in nlp(annotation["caption"])]
+                image = images[annotation["image_id"]]
+                label = "yes" if annotation["foil_word"] == "ORIG" else "no"
+                writer.writerow([label, " ".join(caption), image])
